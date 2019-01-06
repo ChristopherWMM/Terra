94a452dad1c358994d952fdefeb1b490a74cee51
ChristopherWMM <christopherwmm@gmail.com>
2019-01-05 23:58:49 -0800

    Finish noise blending improvements

--- me/christopherwmm/terra/noise/BlendMode.java
+++ me/christopherwmm/terra/noise/BlendMode.java
@@ -7,24 +7,29 @@ package me.christopherwmm.terra.noise;
  * @author ChristopherWMM
  */
 public enum BlendMode {
+	// Darken
 	Darken((x, y) -> Math.min(x, y)),
 	Multiply((x, y) -> x * y),
 	ColorBurn((x, y) -> (y > 0) ? Math.max(1 - (1 - x) / y, 0) : 0),
 	LinearBurn((x, y) -> Math.max(x + y - 1, 0)),
 
+	// Lighten
 	Lighten((x, y) -> Math.max(x, y)),
-	// ColorDodge((x, y) -> (x < 1) ? y / (1 - x) : 1),
-	// LinearDodge((x, y) -> Math.min(x + y, 1)),
+	ColorDodge((x, y) -> (y < 1) ? Math.min(x / (1 - y), 1) : 1),
+	LinearDodge((x, y) -> Math.min(x + y, 1)),
 	Screen((x, y) -> 1 - (1 - x) * (1 - y)),
+
+	// Contrast
 	Overlay((x, y) -> (x <= 0.5) ? (2 * x * y) : (1 - 2 * (1 - x) * (1 - y))),
-
 	SoftLight((x, y) -> (y <= 0.5) ? x * (y + 0.5) : 1 - (1 - x) * (1 - (y - 0.5))),
-	// HardLight((x, y) -> (y <= 0.5) ? 2 * x * y : 1 - (1 - x) * (1 - 2 * (y - 0.5))),
-	// VividLight((x, y) -> (y <= 0.5) ? (y > 0) ? Math.max(1 - (1 - x) / (2 * y), 0) : 0 : (x < 1) ? y / (2 *(1 - x)) : 1),
-	// LinearLight((x, y) -> (y <= 0.5) ? 1 - 2 * y + x : x + 2 * (y - 0.5)),
-	// PinLight((x, y) -> (y <= 0.5) ? Math.min(x, 2 * y) : Math.max(x, 2 * (y - 0.5))),
+	HardLight((x, y) -> (y <= 0.5) ? 2 * x * y : 1 - (1 - x) * (1 - 2 * (y - 0.5))),
+	VividLight((x, y) -> (y <= 0.5) ? ((y != 0) ? Math.max(1 - (1 - x) / (2 * y), 0) : 0) : ((y != 1) ? Math.min(x / (2 * (1 - y)), 1) : 1)),
+	LinearLight((x, y) -> Math.max(Math.min(x + 2 * y - 1, 1), 0)),
+	PinLight((x, y) -> (y <= 0.5) ? Math.min(x, 2 * y) : Math.max(x, 2 * (y - 0.5))),
+	HardMix((x, y) -> (y < 1 - x) ? 0 : 1),
 
-	Difference((x, y) -> Math.abs(x - y)),
+	// Comparative
+	Difference((x, y) -> Math.abs(y - x)),
 	Exclusion((x, y) -> 0.5 - 2 * (x - 0.5) * (y - 0.5));
 
 	private final BlendCalculator calculator;
